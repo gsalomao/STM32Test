d643c8408246d6828d743377e556b557c522282c
Gustavo Salomão <gsalomao.eng@gmail.com>
2018-09-26 21:25:46 -0300

    Add Finite-State Machine
    Add Finite-State Machine to handle the following states:
       - STATE A
       - STATE B
       - STATE C
       - STATE D

--- src/main.c
+++ src/main.c
@@ -4,12 +4,65 @@
 
 #include "stm32f4xx_hal.h"
 
+typedef enum
+{
+    FSM_STATE_A,
+    FSM_STATE_B,
+    FSM_STATE_C,
+    FSM_STATE_D
+
+} FSM_STATE_t;
+
+static inline void RunFSM(void);
+
 int main(void)
 {
-    HAL_Init();
-
     while (1)
-        ;
+    {
+        /* Run the Finite-State Machine */
+        RunFSM();
+    }
 
     return 0;
+}
+
+/**
+ * Run the Finite-State Machine.
+ *
+ * @note    The Finite-State Machine is composed by 4 states
+ *          (see @ref FSM_STATE_t).
+ */
+static inline void RunFSM(void)
+{
+    static FSM_STATE_t state = FSM_STATE_A;
+
+    switch (state)
+    {
+        default:
+        case FSM_STATE_A: /* Initialize MCU peripherals */
+            HAL_Init();
+
+            state = FSM_STATE_B;
+            break;
+
+        case FSM_STATE_B: /* Initialize LIS2DE12TR */
+            /* TODO: Initialize the temperature sensor */
+
+            state = FSM_STATE_C;
+            break;
+
+        case FSM_STATE_C: /* Read temperature sensor */
+            /* TODO: Read temperature from sensor */
+
+            /* TODO: Store temperature in circular buffer */
+
+            state = FSM_STATE_D;
+            break;
+
+        case FSM_STATE_D: /* Sleep wait for next cycle */
+            /* TODO: Enter in sleep mode */
+
+            state = FSM_STATE_C;
+            break;
+    }
 }
